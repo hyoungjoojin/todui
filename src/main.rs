c7d7d2067ade8552e8fa83db023b00d17d326f12
hyoungjoojin <hyoungjoo.j@gmail.com>
2024-09-21 05:16:53 +0000

    feat: add implementation for loading api token

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 mod canvas;
 mod controller;
+mod utils;
 mod view;
 
 use crate::{
     canvas::Canvas,
     controller::{state::State, Controller},
+    utils::security::get_api_token,
     view::View,
 };
 
 fn main() {
+    let api_token = match get_api_token() {
+        Ok(token) => token,
+        Err(_) => {
+            println!("Your Todoist API token could not be found by todui.\n");
+            println!("Export or set an environment variable.");
+            println!("  export TODOIST_API_TOKEN=\"YOUR_API_TOKEN\"");
+            return;
+        }
+    };
+
     let mut canvas = Canvas::new().expect("");
     let mut view: View = View::new();
     let controller: Controller = Controller::new();
