b19b778fa1c7fa438da9107459c7ee500c1bb653
hyoungjoojin <hyoungjoo.j@gmail.com>
2024-10-13 05:29:03 +0000

    fix: update task section scrolling logic

--- src/app/components/containers/tasks.rs
+++ src/app/components/containers/tasks.rs
@@ -25,35 +25,21 @@ impl Tasks {
             on,
             task_index,
             tasks,
-            filter,
         } = props;
 
-        let task_index = if task_index < tasks.len() {
-            task_index
-        } else {
-            0
-        };
+        let height = area.height as usize - 2;
+
+        let num_tasks = tasks.len();
+        let scroll = if num_tasks > height { true } else { false };
 
         let color = if on { Color::Green } else { Color::White };
 
         let tasks: Vec<Line> = tasks
             .iter()
-            .filter(filter)
             .enumerate()
             .map(|(index, task)| {
                 Line::from(Span::styled(
-                    format!(
-                        "{} {}",
-                        task.content(),
-                        match *task.due() {
-                            Some(due) => {
-                                format!("{}", due.date())
-                            }
-                            None => {
-                                "No due date.".to_string()
-                            }
-                        }
-                    ),
+                    format!("{}", task.content()),
                     if index == task_index {
                         Color::Green
                     } else {
@@ -63,40 +49,75 @@ impl Tasks {
             })
             .collect();
 
-        let scrollbar = Scrollbar::new(ratatui::widgets::ScrollbarOrientation::VerticalRight);
-        let mut scrollbar_state = ScrollbarState::new(tasks.len()).position(task_index);
+        let content = Paragraph::new(Text::from(tasks))
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .style(color)
+                    .title(TITLE),
+            )
+            .scroll(if scroll {
+                calculate_scroll_offset(task_index, num_tasks, height)
+            } else {
+                (0, 0)
+            });
 
-        frame.render_widget(
-            Paragraph::new(Text::from(tasks))
-                .block(
-                    Block::default()
-                        .borders(Borders::ALL)
-                        .style(color)
-                        .title(TITLE),
-                )
-                .scroll((task_index as u16, 0)),
-            area,
-        );
-        frame.render_stateful_widget(
-            scrollbar,
-            area.inner(Margin {
-                vertical: 1,
-                horizontal: 0,
-            }),
-            &mut scrollbar_state,
-        );
+        frame.render_widget(content, area);
+
+        if scroll {
+            let scrollbar = Scrollbar::new(ratatui::widgets::ScrollbarOrientation::VerticalRight)
+                .begin_symbol(Some("↑"))
+                .end_symbol(Some("↓"))
+                .thumb_symbol("▒");
+
+            let mut scrollbar_state = ScrollbarState::new(num_tasks - height / 2)
+                .position(calculate_scrollbar_position(task_index, height));
+
+            frame.render_stateful_widget(
+                scrollbar,
+                area.inner(Margin {
+                    vertical: 1,
+                    horizontal: 0,
+                }),
+                &mut scrollbar_state,
+            );
+        }
+
+        fn calculate_scroll_offset(index: usize, length: usize, height: usize) -> (u16, u16) {
+            let half_height = height / 2;
+
+            let vertical_offset = if index < half_height {
+                0
+            } else if length - index < half_height {
+                length - height
+            } else {
+                index - half_height
+            };
+            let horizontal_offset = 0;
+
+            (vertical_offset as u16, horizontal_offset as u16)
+        }
+
+        fn calculate_scrollbar_position(index: usize, height: usize) -> usize {
+            let half_height = height / 2;
+
+            if index < half_height {
+                0
+            } else {
+                index - half_height
+            }
+        }
     }
 }
 
 pub struct TasksProps<'a> {
     on: bool,
     task_index: usize,
-    tasks: &'a Vec<Task>,
-    filter: Box<dyn Fn(&&Task) -> bool + 'a>,
+    tasks: Vec<&'a Task>,
 }
 
-impl<'a> From<(&'a Model, &Context)> for TasksProps<'a> {
-    fn from((model, context): (&'a Model, &Context)) -> TasksProps<'a> {
+impl<'a> From<(&'a Model, &mut Context)> for TasksProps<'a> {
+    fn from((model, context): (&'a Model, &mut Context)) -> TasksProps<'a> {
         let on = context.stage() != Stage::SIDEBAR;
 
         let project = model.projects().get(context.project_index());
@@ -116,11 +137,20 @@ impl<'a> From<(&'a Model, &Context)> for TasksProps<'a> {
             }),
         };
 
+        let tasks: Vec<&Task> = model.tasks().iter().filter(filter).collect();
+
+        context.set_task_index(if tasks.len() == 0 {
+            0
+        } else if context.task_index() >= tasks.len() {
+            tasks.len() - 1
+        } else {
+            context.task_index()
+        });
+
         TasksProps {
             on,
             task_index: context.task_index(),
-            tasks: model.tasks(),
-            filter,
+            tasks,
         }
     }
 }
