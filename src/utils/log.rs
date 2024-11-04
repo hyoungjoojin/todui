3eafe833053606356f8d063caed9e9ed3fe4d3d8
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-11-04 14:08:23 +0900

    feat: add limit to number of stored log files (#48)

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -1,15 +1,25 @@
+use chrono::Local;
+use dirs::home_dir;
 use std::{
-    fs::{create_dir_all, File},
+    fs::{self, create_dir_all, File},
+    path::PathBuf,
     process::exit,
 };
 
-use chrono::Local;
-use dirs::home_dir;
-
 const LOG_DIRECTORY: &str = ".local/share/todui/logs";
 const LOG_FILENAME_FORMAT: &str = "%Y-%m-%d_%H-%M-%S.log";
+const LOG_FILE_COUNT_LIMIT: usize = 10;
 
 pub fn initialize_log_file() -> File {
+    let directory = get_log_directory();
+
+    clear_log_directory(&directory);
+
+    let file = directory.join(Local::now().format(LOG_FILENAME_FORMAT).to_string());
+    File::create(file).unwrap()
+}
+
+fn get_log_directory() -> PathBuf {
     let directory = match home_dir() {
         Some(dir) => dir,
         None => {
@@ -23,5 +33,23 @@ pub fn initialize_log_file() -> File {
         create_dir_all(&directory).expect("");
     }
 
-    File::create(directory.join(Local::now().format(LOG_FILENAME_FORMAT).to_string())).unwrap()
+    directory
+}
+
+fn clear_log_directory(directory: &PathBuf) {
+    let file_count = match fs::read_dir(directory) {
+        Ok(paths) => paths
+            .into_iter()
+            .filter(|path| match path {
+                Ok(path) => path.path().is_file(),
+                Err(_) => false,
+            })
+            .count(),
+        Err(_) => 0,
+    };
+
+    if file_count > LOG_FILE_COUNT_LIMIT {
+        fs::remove_dir_all(directory).unwrap();
+        fs::create_dir(directory).unwrap();
+    }
 }
